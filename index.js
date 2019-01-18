a46ef5c48e4526115ee25dc872a423b2ecae75d6
princel3lcS <princeboy__@hotmail.com>
2019-01-18 10:59:29 +0700

    add access token

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { db } = require('./config/firebaseConfig')
 const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const axios = require('axios')
+const querystring = require('querystring')
 
 const PORT = process.env.PORT || 5000
 app.use(cors())
+app.use(bodyParser.json({
+  type: '*/*'
+}))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -24,6 +30,25 @@ app.get('/getuser', async (req, res) => {
     status: 200,
     results
   })
+})
+
+app.post('/line-token', async (req, res) => {
+  const {code, redirect_uri} = req.body
+  const data = {
+    grant_type: 'authorization_code',
+    client_id: '1635880494',
+    client_secret: 'd105fd17568b15c6f427b1d1974586e5',
+    code,
+    redirect_uri
+  }
+
+  const options = {
+    method: 'POST',
+    data: querystring.stringify(data),
+    url: 'https://api.line.me/oauth2/v2.1/token'
+  }
+  const results = await axios(options)
+  res.send(results.data)
 })
 
 app.listen(PORT, () => console.log('application is listening on:', PORT))
